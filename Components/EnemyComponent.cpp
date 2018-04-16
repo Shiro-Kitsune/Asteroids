658f90e37ae996ba522f9aa883f98500ae3ddee6
Shiro-Kitsune <messir.x@gmail.com>
2018-04-15 23:22:13 -0400

    Update EnemyComponent.cpp

--- Components/EnemyComponent.cpp
+++ Components/EnemyComponent.cpp
@@ -1,38 +1,28 @@
 #include "stdafx.h"
 
-EnemyComponent::EnemyComponent(int pointsWorth, const std::initializer_list<ItemType> & list) : m_pointsWorth(pointsWorth)
+EnemyComponent::EnemyComponent(int pointsWorth) : m_pointsWorth(pointsWorth)
 {
-	int numOfLootTypes = list.size();
-	
-	if(numOfLootTypes != 0)
+	float num = Utilities::Random(0.0f, 1.0f);
+	if (num < 0.9f)
 	{
-		float probabilityStep = 1.0f / numOfLootTypes;
-
-		float num = Utilities::Random(0.0f, 1.0f);
-		if(num < 0.15)
+		m_type = ItemType::None;
+	}
+	else
+	{
+		num = Utilities::Random(0.0f, 1.0f);
+		if (num < 0.1f)
 		{
-			num = Utilities::Random(0.0f, 1.0f);
-
-			int count = 1;
-			for (auto &element : list)
-			{
-				if(num < probabilityStep * count)
-				{
-					m_type = element;
-					break;
-				}
-				++count;
-			}
+			m_type = ItemType::Heart;
+		}
+		else if (num < 0.35)
+		{
+			m_type = ItemType::PowerUp;
 		}
 		else
 		{
-			m_type = ItemType::None;
+			m_type = ItemType::Mineral;
 		}
-    }
-    else
-    {
-    	m_type = ItemType::None;
-    }
+	}
 }
 
 int EnemyComponent::GetPointsWorth() const
